d20dd37835c816e1495730b48f414d359c7c2114
venqka <venelinborisov5@gmail.com>
2019-04-26 15:31:50 +0300

    pass hovered category slug to ajax

--- scripts.js
+++ scripts.js
@@ -7,10 +7,25 @@ jQuery(document).ready(function(){
     	jQuery('.max-mega-menu li').hover(function(){
 
 	    	var target = jQuery(this);
-
 	    	var targetUrl = target.find('a').attr('href');
+	    	var urlArray = targetUrl.split('/');
 
-			jQuery('.dnmmm-widget-content-wrap').html(targetUrl);
+			var slug = urlArray[urlArray.length-2];
+	  		
+	    	jQuery.ajax({
+	        	url: dnmmm_ajax.ajax_url,
+	        	type: 'POST',
+	        	data: {
+					action: 'get_category_info',
+					categoryUrl: slug
+		        },
+		        success : function(category_info) {
+		        	jQuery('.dnmmm-widget-content-wrap').html(category_info);
+		        },
+		        error : function(error) {
+					console.log(error);
+				}
+			});
 
 		});//on li hover
     
