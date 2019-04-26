42330a1a8dd4c84a95e558aa3979f67fc871a700
venqka <venelinborisov5@gmail.com>
2019-04-26 15:31:01 +0300

    Get the category info by slug

--- mmm-product-category-widget.php
+++ mmm-product-category-widget.php
@@ -32,3 +32,52 @@ function dn_mmm_enqueue() {
 }
 add_action( 'wp_enqueue_scripts', 'dn_mmm_enqueue' );
 
+function get_category_info() {
+
+	$category_slug = $_POST["categoryUrl"];
+
+	if( !empty( $category_slug ) ) {
+	
+		$product_category = get_term_by( 'slug', $category_slug, 'product_cat' );
+		$category_name = $product_category->name;
+		$category_description = $product_category->description;
+		$category_id = $product_category->term_id;
+		$category_thumbnail_id = get_woocommerce_term_meta( $category_id, 'thumbnail_id', true );
+		$category_thumbnail = wp_get_attachment_url( $category_thumbnail_id );
+		ob_start();
+?>
+			<div class="mmm-product-category">
+				<div class="mmm-product-category-name">
+					<h2><?php echo $category_name; ?></h2>
+				</div>
+<?php
+				if( !empty( $category_description ) ) {
+?>				
+					<div class="mmm-product-category-description">
+						<?php echo $category_description; ?>
+					</div>
+<?php
+				}
+
+				if( !empty( $category_thumbnail ) ) {
+?>				
+					<div class="mmm-product-category-thumbnail">
+						<img src="<?php echo $category_thumbnail; ?>" />
+					</div>
+<?php
+				}
+?>						
+			</div>	
+<?php	
+
+		$category_info = ob_get_clean();	
+
+		wp_send_json( $category_info );
+
+	}//if we have category slug	
+	
+	wp_die();
+}
+add_action( 'wp_ajax_get_category_info', 'get_category_info' );
+add_action( 'wp_ajax_nopriv_get_category_info', 'get_category_info' );
+
