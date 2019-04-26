d0a4e5ecb6291c13b869075b5b72ca4cbd1f0133
venqka <venelinborisov5@gmail.com>
2019-04-26 12:37:24 +0300

    Get product category url on hover

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 jQuery(document).ready(function(){
 	
+	var container = jQuery('.dnmmm-widget-content-wrap');
+
+	// jQuery('li.mega-menu-item').on('open_panel', function() {
+    	
+    	jQuery('.max-mega-menu li').hover(function(){
+
+	    	var target = jQuery(this);
+
+	    	var targetUrl = target.find('a').attr('href');
+
+			jQuery('.dnmmm-widget-content-wrap').html(targetUrl);
+
+		});//on li hover
+    
+    // });//on open panel
+	
+
 });
