5c8821a421436fb36e3db3c9f741197d64b59e65
venqka <venelinborisov5@gmail.com>
2019-04-26 11:48:10 +0300

    Initial commit

---
+++ mmm-product-category-widget.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Max Mega Menu Product Category Widget
+Plugin URI: devnecks.com
+Description: This plugin adds product category widget for Max Mega Menu
+Author: venqka
+Author URI: devnecks.com
+Version: 1.0
+*/
+
