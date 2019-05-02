bd4079d85f885ac3598b19af27d15bd2d5644619
venqka <venelinborisov5@gmail.com>
2019-05-02 10:43:06 +0300

    Execute scipts on hoverIntent

--- scripts.js
+++ scripts.js
@@ -4,28 +4,32 @@ jQuery(document).ready(function(){
 
 	// jQuery('li.mega-menu-item').on('open_panel', function() {
     	
-    	jQuery('.max-mega-menu li').hover(function(){
+    	jQuery('.max-mega-menu li').hoverIntent(function(){
 
 	    	var target = jQuery(this);
-	    	var targetUrl = target.find('a').attr('href');
-	    	var urlArray = targetUrl.split('/');
+	    	
+	    	if(target.hasClass('mega-menu-item-object-product_cat') ) {
+	    	
+		    	var targetUrl = target.find('a').attr('href');
+		    	var urlArray = targetUrl.split('/');
 
-			var slug = urlArray[urlArray.length-2];
-	  		
-	    	jQuery.ajax({
-	        	url: dnmmm_ajax.ajax_url,
-	        	type: 'POST',
-	        	data: {
-					action: 'get_category_info',
-					categoryUrl: slug
-		        },
-		        success : function(category_info) {
-		        	jQuery('.dnmmm-widget-content-wrap').html(category_info);
-		        },
-		        error : function(error) {
-					console.log(error);
-				}
-			});
+				var slug = urlArray[urlArray.length-2];
+		  		
+		    	jQuery.ajax({
+		        	url: dnmmm_ajax.ajax_url,
+		        	type: 'POST',
+		        	data: {
+						action: 'get_category_info',
+						categoryUrl: slug
+			        },
+			        success : function(category_info) {
+			        	jQuery('.dnmmm-widget-content-wrap').html(category_info);
+			        },
+			        error : function(error) {
+						console.log(error);
+					}
+				});
+			}	
 
 		});//on li hover
     
